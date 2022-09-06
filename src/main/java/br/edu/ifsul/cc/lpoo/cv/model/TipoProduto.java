5f2e198dd8667c4f7cf39317a08cc2ff9ec83cc6
decesarojunior <decesarojunior@yahoo.com.br>
2022-09-06 10:42:14 -0300

    implementação do diagrama de classes e anotações jpa.

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/model/TipoProduto.java
@@ -0,0 +1,11 @@
+
+package br.edu.ifsul.cc.lpoo.cv.model;
+
+/**
+ *
+ * @author telmo
+ */
+public enum TipoProduto {
+    
+    MEDICAMENTO, SESSAO_FISIOTERAPIA, SESSAO_ADESTRAMENTO, ATENDIMENTO_AMBULATORIAL, CONSULTA, CONSULTA_REVISAO
+}
