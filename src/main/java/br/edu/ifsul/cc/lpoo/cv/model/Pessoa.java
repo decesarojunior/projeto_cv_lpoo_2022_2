b378211b2aa85fdcc6e2b6294c0c8b8b2accd0e3
decesarojunior <decesarojunior@yahoo.com.br>
2022-09-06 10:46:54 -0300

    implementação do diagrama de classes e anotações jpa.

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/cv/model/Pessoa.java
@@ -0,0 +1,239 @@
+
+package br.edu.ifsul.cc.lpoo.cv.model;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+/**
+ *
+ * @author telmo
+ */
+
+@Entity
+@Table(name = "tb_pessoa")
+@Inheritance(strategy = InheritanceType.JOINED)//alterar a estratégia.
+@DiscriminatorColumn(name = "tipo")
+public abstract class Pessoa implements Serializable {
+    
+    @Id
+    private String cpf;
+    
+    @Column(nullable = false, length = 50)
+    private String rg; 
+    
+    @Column(nullable = false, length = 100)
+    private String nome;
+    
+    @Column(nullable = false, length = 100)
+    private String senha;
+    
+    @Column(nullable = true, length = 11)
+    private String numero_celular;
+    
+    @Column(nullable = true, length = 100)
+    private String email;
+        
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar data_cadastro;
+    
+    @Column(nullable = true)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar data_nascimento;
+    
+    @Column(nullable = true, length = 8)
+    private String cep;
+    
+    @Column(nullable = true, length = 100)
+    private String endereco;
+    
+    @Column(nullable = true, length = 100)
+    private String complemento;
+    
+    @Transient
+    private String tipo;    
+    
+    
+    public Pessoa(){
+        
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the rg
+     */
+    public String getRg() {
+        return rg;
+    }
+
+    /**
+     * @param rg the rg to set
+     */
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the numero_celular
+     */
+    public String getNumero_celular() {
+        return numero_celular;
+    }
+
+    /**
+     * @param numero_celular the numero_celular to set
+     */
+    public void setNumero_celular(String numero_celular) {
+        this.numero_celular = numero_celular;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the data_cadastro
+     */
+    public Calendar getData_cadastro() {
+        return data_cadastro;
+    }
+
+    /**
+     * @param data_cadastro the data_cadastro to set
+     */
+    public void setData_cadastro(Calendar data_cadastro) {
+        this.data_cadastro = data_cadastro;
+    }
+
+    /**
+     * @return the data_nascimento
+     */
+    public Calendar getData_nascimento() {
+        return data_nascimento;
+    }
+
+    /**
+     * @param data_nascimento the data_nascimento to set
+     */
+    public void setData_nascimento(Calendar data_nascimento) {
+        this.data_nascimento = data_nascimento;
+    }
+
+    /**
+     * @return the cep
+     */
+    public String getCep() {
+        return cep;
+    }
+
+    /**
+     * @param cep the cep to set
+     */
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    /**
+     * @return the complemento
+     */
+    public String getComplemento() {
+        return complemento;
+    }
+
+    /**
+     * @param complemento the complemento to set
+     */
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+}
